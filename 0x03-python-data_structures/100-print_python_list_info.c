1609007173e40d060329fde53c00a66d1526e146
“abdullah <abdullah.h.elgabry@gmail.com>
2024-01-08 16:17:44 +0200

    0x03 task adv

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -6,22 +6,23 @@
  * @p: lst
  */
 
+
 void print_python_list_info(PyObject *p)
 {
-	int hght, j , plc;
+	int hgth, x, plc;
 	PyObject *obj;
 
-	hght = rng(p);
+	hgth = Py_SIZE(p);
 	plc = ((PyListObject *)p)->allocated;
 
-	printf("[*] Size of the Python List = %d\n", hght);
+	printf("[*] Size of the Python List = %d\n", hgth);
 	printf("[*] Allocated = %d\n", plc);
 
-	for (j = 0; j < hght; j++)
+	for (x = 0; x < hgth; x++)
 	{
-		printf("Element %d: ", j);
+		printf("Element %d: ", x);
 
-		obj = PyList_GetItem(p, j);
+		obj = PyList_GetItem(p, x);
 		printf("%s\n", Py_TYPE(obj)->tp_name);
 	}
 }
