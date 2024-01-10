dc79a5adbf708f62a91e57b5acf4804c1ebf03e8
“abdullah <abdullah.h.elgabry@gmail.com>
2024-01-10 13:49:40 +0200

    task 0x04

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
-
 def roman_to_int(roman_string):
-    if roman_string is None or type(roman_string) is not str:
-        return (0)
-    
-    strom = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    dig = 0
-    for r in range(len(roman_string)):
-        if r > 0 and strom[roman_string[r]] > strom[roman_string[r - 1]]:
-            dig += strom[roman_string[r]] - 2 * strom[roman_string[r - 1]]
-        else:
-            dig += strom[roman_string[r]]
-    return (dig)
+    if not isinstance(roman_string, str):
+        return 0
+    total = 0
+    num = 0
+    digits = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    for r in reversed(roman_string):
+        num = digits[r]
+        total += num if total < num * 5 else -num
+    return total
