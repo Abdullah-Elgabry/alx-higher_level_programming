d0c68fd4b8eff2874c903bbf767d686759a07c5f
“abdullah <abdullah.h.elgabry@gmail.com>
2024-01-10 13:54:23 +0200

    task 0x04

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
+
 def roman_to_int(roman_string):
     if not isinstance(roman_string, str):
         return 0
-    total = 0
+    res = 0
     digt = 0
     romdig = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     for i in reversed(roman_string):
         digt = romdig[i]
-        total += digt if total < digt * 5 else -digt
-    return total
+        res += digt if res < digt * 5 else -digt
+    return res
