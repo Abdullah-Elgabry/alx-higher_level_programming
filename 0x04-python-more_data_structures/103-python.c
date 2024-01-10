46d27b4b63ccd1239f8ce09ffab85e240f80327a
“abdullah <abdullah.h.elgabry@gmail.com>
2024-01-10 14:07:05 +0200

    task 0x04

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,42 +1,6 @@
 #include <stdio.h>
 #include <Python.h>
 
-
-
-/**
- * print_python_list - this func willl prt
- * lst info
- *
- * @p: obj for py
- *
- * Return: N/A
- */
-
-
-void print_python_list(PyObject *p)
-{
-	long int size, x;
-	PyObject *my_object;
-	PyListObject *my_lst;
-
-	size = ((PyVarObject *)(p))->ob_size;
-	my_lst = (PyListObject *)p;
-
-	printf("[*] Python list info\n");
-	printf("[*] Size of the Python List = %ld\n", size);
-	printf("[*] Allocated = %ld\n", my_lst->allocated);
-
-	for (x = 0; x < size; x++)
-	{
-		my_object = ((PyListObject *)p)->ob_item[x];
-		printf("Element %ld: %s\n", x, ((my_object)->ob_type)->tp_name);
-		if (PyBytes_Check(my_object))
-			print_python_bytes(my_object);
-	}
-}
-
-
-
 /**
  * print_python_bytes - this func will prt py byts
  *
@@ -44,7 +8,6 @@ void print_python_list(PyObject *p)
  *
  * Return: N/A
  */
-
 
 void print_python_bytes(PyObject *p)
 {
@@ -78,4 +41,38 @@ void print_python_bytes(PyObject *p)
 			printf(" %02x", 256 + string[x]);
 
 	printf("\n");
+}
+
+
+
+/**
+ * print_python_list - this func willl prt
+ * lst info
+ *
+ * @p: obj for py
+ *
+ * Return: N/A
+ */
+
+
+void print_python_list(PyObject *p)
+{
+	long int size, x;
+	PyObject *my_object;
+	PyListObject *my_lst;
+
+	size = ((PyVarObject *)(p))->ob_size;
+	my_lst = (PyListObject *)p;
+
+	printf("[*] Python list info\n");
+	printf("[*] Size of the Python List = %ld\n", size);
+	printf("[*] Allocated = %ld\n", my_lst->allocated);
+
+	for (x = 0; x < size; x++)
+	{
+		my_object = ((PyListObject *)p)->ob_item[x];
+		printf("Element %ld: %s\n", x, ((my_object)->ob_type)->tp_name);
+		if (PyBytes_Check(my_object))
+			print_python_bytes(my_object);
+	}
 }
