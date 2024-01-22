bbbace0e3db734a52efa76cb21e5c8d7b81e6d79
“abdullah <abdullah.h.elgabry@gmail.com>
2024-01-22 21:32:26 +0200

    0x05 task

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -3,6 +3,6 @@ import sys
 def safe_function(fct, *args):
     try:
         return fct(*args)
-    except Exception as exp:
-        print("Exception: {}".format(exp), file=sys.stderr)
+    except Exception as e:
+        print("Exception: {}".format(e), file=sys.stderr)
         return None
