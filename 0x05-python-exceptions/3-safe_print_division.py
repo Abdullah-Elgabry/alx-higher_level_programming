6ba3eba6c8fe873b953a2daafde23abd763074c3
“abdullah <abdullah.h.elgabry@gmail.com>
2024-01-22 21:06:12 +0200

    0x05 task

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
-    div = 0
+    result = 0
 
     try:
-        div = a / b
+        result = a / b
     except ZeroDivisionError:
-        div = None
+        result = None
     finally:
-        print('Inside div: {}'.format(div))
-        return div
+        print('Inside result: {}'.format(result))
+        return result
