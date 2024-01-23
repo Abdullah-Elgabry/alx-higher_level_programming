552496c46e2c881c7d20685410eaa42d7183a0a6
“abdullah <abdullah.h.elgabry@gmail.com>
2024-01-24 01:38:41 +0200

    0x06. Python task

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -5,14 +5,12 @@
 class Square:
     """Defining a class Square."""
 
-    def __init__(self, size=0, position=(0, 0)):
+    def __init__(self, size=0):
         """INIT instance.
         Args:
             size (int): represent the square size.
-            position (int, int): square position.
         """
         self.__size = size
-        self.position = position
 
     @property
     def size(self):
@@ -31,20 +29,6 @@ class Square:
         elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
-
-    @property
-    def position(self):
-        """square position."""
-        return (self.__position)
-
-    @position.setter
-    def position(self, value):
-        if (not isinstance(value, tuple) or
-                len(value) != 2 or
-                not all(isinstance(num, int) for num in value) or
-                not all(num >= 0 for num in value)):
-            raise TypeError("position must be a tuple of 2 positive integers")
-        self.__position = value
 
     def area(self):
         """Get the square area.
@@ -83,14 +67,3 @@ class Square:
             [print(" ", end="") for j in range(0, self.__position[0])]
             [print("#", end="") for k in range(0, self.__size)]
             print("")
-
-    def __str__(self):
-        """Defining square print method."""
-        if self.__size != 0:
-            [print("") for i in range(0, self.__position[1])]
-        for i in range(0, self.__size):
-            [print(" ", end="") for j in range(0, self.__position[0])]
-            [print("#", end="") for k in range(0, self.__size)]
-            if i != self.__size - 1:
-                print("")
-        return ("")
