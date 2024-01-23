f63984d6873c58ba716fc4fb9d5aff46d1938baa
“abdullah <abdullah.h.elgabry@gmail.com>
2024-01-24 00:58:51 +0200

    0x06. Python task

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python3
+"""Defining a Square module"""
+
+
+class Square:
+    """Defining a class Square."""
+
+    def __init__(self, size=0):
+        """INIT instance.
+        Args:
+            size (int): represent the square size.
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """the Prop for the square side length.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+
+    @property
+    def position(self):
+        """square position."""
+        return (self.__position)
+
+    @position.setter
+    def position(self, value):
+        if (not isinstance(value, tuple) or
+                len(value) != 2 or
+                not all(isinstance(num, int) for num in value) or
+                not all(num >= 0 for num in value)):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def area(self):
+        """get square function.
+
+        Returns:
+            The size square size.
+        """
+        return self.__size ** 2
+
+    def my_print(self):
+        """this func will draw the square by #."""
+        if self.__size == 0:
+            print("")
+            return
+
+        [print("") for x in range(0, self.__position[1])]
+        for x in range(0, self.__size):
+            [print(" ", end="") for j in range(0, self.__position[0])]
+            [print("#", end="") for k in range(0, self.__size)]
+            print("")
