d90cde6107368d43b4a11c8bcd095546c81abdf5
“abdullah <abdullah.h.elgabry@gmail.com>
2024-01-24 01:35:25 +0200

    0x06. Python task

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,96 @@
+#!/usr/bin/python3
+"""Defining a Square module"""
+
+
+class Square:
+    """Defining a class Square."""
+
+    def __init__(self, size=0, position=(0, 0)):
+        """INIT instance.
+        Args:
+            size (int): represent the square size.
+            position (int, int): square position.
+        """
+        self.__size = size
+        self.position = position
+
+    @property
+    def size(self):
+        """the Prop for the square side length.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        """square position."""
+        return (self.__position)
+
+    @position.setter
+    def position(self, value):
+        if (not isinstance(value, tuple) or
+                len(value) != 2 or
+                not all(isinstance(num, int) for num in value) or
+                not all(num >= 0 for num in value)):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def area(self):
+        """Get the square area.
+
+        Returns:
+            square size.
+        """
+        return f"{self.__size ** 2}"
+
+    def __eq__(self, other):
+        return self.area() == other.area()
+
+    def __ne__(self, other):
+        return self.area() != other.area()
+
+    def __gt__(self, other):
+        return self.area() > other.area()
+
+    def __ge__(self, other):
+        return self.area() >= other.area()
+
+    def __lt__(self, other):
+        return self.area() < other.area()
+
+    def __le__(self, other):
+        return self.area() <= other.area()
+
+    def my_print(self):
+        """this func will draw the square by #."""
+        if self.__size == 0:
+            print("")
+            return
+
+        [print("") for x in range(0, self.__position[1])]
+        for x in range(0, self.__size):
+            [print(" ", end="") for j in range(0, self.__position[0])]
+            [print("#", end="") for k in range(0, self.__size)]
+            print("")
+
+    def __str__(self):
+        """Defining square print method."""
+        if self.__size != 0:
+            [print("") for i in range(0, self.__position[1])]
+        for i in range(0, self.__size):
+            [print(" ", end="") for j in range(0, self.__position[0])]
+            [print("#", end="") for k in range(0, self.__size)]
+            if i != self.__size - 1:
+                print("")
+        return ("")
