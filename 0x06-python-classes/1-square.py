cb17910a943c30982ed3aea721b715b78e3c5dbf
“abdullah <abdullah.h.elgabry@gmail.com>
2024-01-23 23:14:09 +0200

    0x06. Python task

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Defining a Square module"""
+
+class Square:
+    """Defining a class Square."""
+
+    def __init__(self, size):
+        """INIT instance.
+        Args:
+            size (int): represent the square size.
+        """
+        self.__size = size
