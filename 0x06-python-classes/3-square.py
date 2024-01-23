e750d17059d10d36d3dba62912d93b84bdd63acf
“abdullah <abdullah.h.elgabry@gmail.com>
2024-01-23 23:30:37 +0200

    0x06. Python task

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Defining a Square module"""
+
+
+class Square:
+    """Defining a class Square."""
+
+    def __init__(self, size=0):
+        """INIT instance.
+        Args:
+            size (int): represent the square size.
+        Raises:
+            TypeError: If the size is not an integer
+            ValueError: If size is < 0
+        """
+        self.__size = size
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        
+    def area(self):
+        """
+        this func will Returns the current area of the square.
+        """
+        return f"{self.__size **2}"
