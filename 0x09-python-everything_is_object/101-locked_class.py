8729abba25e18157ea4809cf73224dbe6780055b
“abdullah <abdullah.h.elgabry@gmail.com>
2024-01-31 02:27:21 +0200

    0x09 task

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""this func for the locked class."""
+
+
+class LockedClass:
+    """
+    this func will stores the first name to filter the users.
+    """
+
+    __slots__ = ["first_name"]
