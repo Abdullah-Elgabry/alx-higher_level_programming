7ddcd067287db9961d2f6af7894df00fa971f6cb
“abdullah <abdullah.h.elgabry@gmail.com>
2024-02-05 22:19:47 +0200

    inherit task

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+'''this if for th is_same_class function.'''
+
+
+def is_same_class(obj, a_class):
+    '''this func will if obj = class instance '''
+    return type(obj) == a_class
