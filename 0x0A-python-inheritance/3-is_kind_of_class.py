80418f4a86eb46711503165813834fbd11c964f5
“abdullah <abdullah.h.elgabry@gmail.com>
2024-02-05 22:22:18 +0200

    inherit task

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+'''this is for is_kind_of_class func.'''
+
+
+def is_kind_of_class(obj, a_class):
+    '''this will checks by isinstance func'''
+    return isinstance(obj, a_class)
