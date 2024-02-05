d0ecf113a7f73697d1505182fd3eba7ec2783736
“abdullah <abdullah.h.elgabry@gmail.com>
2024-02-05 23:04:06 +0200

    inherit task

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+'''this is for square class'''
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    '''this inherit class id for square data'''
+    def __init__(self, size):
+        '''this is the func Constructor.'''
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        '''this func will get the area square'''
+        return self.__size ** 2
+
+    def __str__(self):
+        '''this func will ret the square with symp'''
+        return "[Square] " + str(self.__size) + "/" + str(self.__size)
