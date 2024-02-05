35db4fcf7d267bbb3d52015f4d2a928a31291a99
“abdullah <abdullah.h.elgabry@gmail.com>
2024-02-05 22:02:09 +0200

    inherit task

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''this is for the lookup function'''
+
+
+def lookup(obj):
+    '''basic function.
+    Args:
+        obj: list obj.
+
+    Returns:
+        all of it's atrributes.
+    '''
+    return dir(obj)
