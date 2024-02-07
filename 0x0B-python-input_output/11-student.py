0ff96d93b1e6973928267a94586b401fb1fd4142
“abdullah <abdullah.h.elgabry@gmail.com>
2024-02-07 02:17:03 +0200

    inherit task

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python3
 """this module for student class"""
 
+
 class Student:
     """this class represent student functions"""
     def __init__(self, first_name, last_name, age):
-        '''constructor'''
+        """constructor"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        '''
+        """
         this will get the dictionary back with all
         student info
-        '''
+        """
         try:
             for plt in attrs:
                 if type(plt) is not str:
@@ -27,9 +28,9 @@ class Student:
         return my_dict
 
     def reload_from_json(self, json):
-        '''
+        """
         this func just replace the value
-        '''
+        """
         for k, v in json.items():
             if k in self.__dict__:
                 self.__dict__[k] = v
