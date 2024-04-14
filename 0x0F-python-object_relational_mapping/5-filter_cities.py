664e69152c8ddf8deb2b824913bbbc70699b3060
“abdullah <abdullah.h.elgabry@gmail.com>
2024-04-14 23:16:42 +0200

    ORM task #Done

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -9,7 +9,7 @@ from sys import argv
 
 if __name__ == '__main__':
     """
-        This will Access to the db , featch the states from db
+    This will Access to the db , featch the states from db
     """
 
     db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
