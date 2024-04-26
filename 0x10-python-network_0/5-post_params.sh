e1d088e3fac821db8802f3423cac308cf765e060
“abdullah <abdullah.h.elgabry@gmail.com>
2024-04-26 21:56:30 +0300

    Network #0 Done

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # this curl script will sends post request and display it.
-curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD"
-                    "$1"
+curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
